5f8217a4aa90c7b7c263a1099a45a06362da3815
Robert Seifert <rseifert.phone@gmail.com>
2015-05-23 06:11:05 -0400

    Added an explosion effect when shooting barrels, Imported from StandardAssets-ParticleSystems

--- Assets/code/scripts/entity/Barrel.cs
+++ Assets/code/scripts/entity/Barrel.cs
@@ -4,11 +4,11 @@ using BuiltBroken.Damage;
 
 public class Barrel : Entity {
 
+	public GameObject effectPrefab;
 	public override bool damageEntity(DamageSource source, float damage)
 	{
 		if (damage > 10) {
 			GoBoom();
-
 		}
 		return base.damageEntity (source, damage);
 	}
@@ -16,6 +16,7 @@ public class Barrel : Entity {
 	void GoBoom()
 	{
 		//TODO set dead, (change color or create sharpnal) and set destroy timer
+		Instantiate (effectPrefab, transform.position, Quaternion.identity);
 		Destroy (gameObject);
 	}
 }

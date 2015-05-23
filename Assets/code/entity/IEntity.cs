7a07a8863553af340aaecb32d79ce7c92bbade5b
Robert Seifert <rseifert.phone@gmail.com>
2015-05-23 03:46:46 -0400

    Relocated scripts to code folder, Added IEntity, Fixed tank not tilting on slanted ground, Changed tank physics

---
+++ Assets/code/entity/IEntity.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Basic interface to be aplied gameobjects that can take damage
+/// </summary>
+public interface IEntity {
+
+	///<summary>
+	///Amount of Heath the entity has left
+	///</summary>
+	///<remarks>Can be used to access the heath of the entity</remarks>
+	///<returns>float value of the hp</returns>
+	float getHeath();
+
+	///<summary>
+	///Directly sets the heath value of the entity
+	///</summary>
+	///<remarks>Shouldn't be used to deal damage</remarks>
+	void setHeath(float amount);
+
+	///<summary>
+	///Used to inflict damage to the entity
+	///</summary>
+	///<remarks>TODO add damage type</remarks>
+	/// <returns>True if the entity was damaged</returns>
+	bool damageEntity(float damage);
+
+	///<summary>
+	///Checks to see if the entity is dead
+	///</summary>
+	/// <returns>True if the entity is dead and waiting to be despawned</returns>
+	bool isDead();
+}
